076c1ef1ca33b2469aa084ac6a72e86b08208b03
Jaffa Aharonov <jaharonov@gmail.com>
2018-02-09 01:46:03 -0600

    first commit

---
+++ server/public/scripts/material.controller.js
@@ -0,0 +1,25 @@
+myApp.controller('MaterialController', function($mdDialog, $mdToast) {
+    console.log('MaterialController');
+
+    var vm = this;
+
+    vm.showToast = function(event) {
+        $mdToast.show(
+            $mdToast.simple()
+                .textContent('Upcoming screenings somewhere on some date!!!')
+                .position('top right')
+                .hideDelay(10000)
+        );
+        // console.log('Clicked!')
+        // $mdDialog.show(
+        //     $mdDialog.alert()
+        //     .parent(angular.element(document.querySelector('#popupContainer')))
+        //     .title('Hi!!!')
+        //     .textContent('You can say other things')
+        //     .ariaLabel('Alert Dialog Demo')
+        //     .ok('Got it!')
+        //     .targetEvent(event)
+        // )
+    }
+    vm.showToast();
+});
